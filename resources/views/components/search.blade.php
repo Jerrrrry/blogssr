7a098d6f1480f450aa9138a48d901e75a37ec54f
Yubin QIU <Yubin.Qiu@kp.org>
2018-12-04 22:47:25 -0800

    yb:add more components

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,7 @@
+<div id="search-wrapper">
+    <form action="#">
+        <input type="text" id="search" placeholder="Search something...">
+        <div id="close-icon"></div>
+        <input class="d-none" type="submit" value="">
+    </form>
+</div>
