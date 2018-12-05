61809a8988f823f2ab537859479e575aad2820f8
Yubin QIU <Yubin.Qiu@kp.org>
2018-12-04 22:31:06 -0800

    yubin:update the blade

---
+++ resources/views/components/breakingnews.blade.php
@@ -0,0 +1,14 @@
+<!-- Breaking News Area -->
+<div class="col-12 col-sm-8">
+    <div class="breaking-news-area">
+        <div id="breakingNewsTicker" class="ticker">
+            <ul>
+              <li><a href="#">你好，LA!</a></li>
+              <li><a href="#">你好，Chino！</a></li>
+              <li><a href="#">你好，好吃的！</a></li>
+              <li><a href="#">你好，好玩的!</a></li>
+              <li><a href="#">你好，所有兔兔们!</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
