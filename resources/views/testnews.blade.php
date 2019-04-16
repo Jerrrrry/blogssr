87599f1e49032232b0f7e91bf656cdfc95e4c550
Yubin QIU <Yubin.Qiu@kp.org>
2019-04-16 01:07:13 +0000

    yubin:update

--- resources/views/testnews.blade.php
+++ resources/views/testnews.blade.php
@@ -5,9 +5,36 @@
 @section('content')
 <div class="container">
     <div class="row">
-      @foreach ($newss as $news)
-        <h2>2</h2>
-      @endforeach
+        <div class="col-12">
+            @foreach ($newss as $news)
+                <div class="single-blog-area blog-style-2 mb-50 wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1000ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 0.4s; animation-name: fadeInUp;">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="single-blog-thumbnail">
+                                    <img src="{{$news['urlToImage']}}" alt="">
+                                    <div class="post-date">
+                                        <a href="#">10 <span>march</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <!-- Blog Content -->
+                                <div class="single-blog-content mt-50">
+                                    <div class="line"></div>
+                                    <a href="#" class="post-tag">Lifestyle</a>
+                                    <h4><a href="#" class="post-headline">{{$news['title']}}</a></h4>
+                                    <p>{{$news['content']}}</p>
+                                    <div class="post-meta">
+                                        <p>By <a href="#">james smith</a></p>
+                                        <p>3 comments</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                                  
+            @endforeach
+        </div>
 
     </div>
 </div>
