10324fe9e162e8f88cc98d4df0354e4da2bacd8f
Yubin QIU <Yubin.Qiu@kp.org>
2019-04-16 21:43:25 +0000

    yubin:update

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -38,39 +38,28 @@
                             <li class="megamenu-item"><a href="#">News</a>
                                         <div class="megamenu">
                                             <ul class="single-mega cn-col-4">
-                                                <li class="title">Headline 1</li>
+                                                <li class="title">News Sources</li>
                                                 @foreach (Helper::sourcesMegamenu()[0] as $source)
-                                                    <li><a href="#">{{$source['name']}}</a></li>
+                                                    <li><a href="/news?source={{$source['id']}}">{{$source['name']}}</a></li>
                                                 @endforeach
-                                                <li><a href="#">Mega Menu Item 1</a></li>
-                                                <li><a href="#">Mega Menu Item 2</a></li>
-                                                <li><a href="#">Mega Menu Item 3</a></li>
-                                                <li><a href="#">Mega Menu Item 4</a></li>
-                                                <li><a href="#">Mega Menu Item 5</a></li>
                                             </ul>
                                             <ul class="single-mega cn-col-4">
-                                                <li class="title">Headline 2</li>
-                                                <li><a href="#">Mega Menu Item 1</a></li>
-                                                <li><a href="#">Mega Menu Item 2</a></li>
-                                                <li><a href="#">Mega Menu Item 3</a></li>
-                                                <li><a href="#">Mega Menu Item 4</a></li>
-                                                <li><a href="#">Mega Menu Item 5</a></li>
+                                                <li class="title"></li>
+                                                @foreach (Helper::sourcesMegamenu()[1] as $source)
+                                                    <li><a href="/news?source={{$source['id']}}">{{$source['name']}}</a></li>
+                                                @endforeach
                                             </ul>
                                             <ul class="single-mega cn-col-4">
-                                                <li class="title">Headline 3</li>
-                                                <li><a href="#">Mega Menu Item 1</a></li>
-                                                <li><a href="#">Mega Menu Item 2</a></li>
-                                                <li><a href="#">Mega Menu Item 3</a></li>
-                                                <li><a href="#">Mega Menu Item 4</a></li>
-                                                <li><a href="#">Mega Menu Item 5</a></li>
+                                                <li class="title"></li>
+                                                @foreach (Helper::sourcesMegamenu()[2] as $source)
+                                                    <li><a href="/news?source={{$source['id']}}">{{$source['name']}}</a></li>
+                                                @endforeach
                                             </ul>
                                             <ul class="single-mega cn-col-4">
-                                                <li class="title">Headline 4</li>
-                                                <li><a href="#">Mega Menu Item 1</a></li>
-                                                <li><a href="#">Mega Menu Item 2</a></li>
-                                                <li><a href="#">Mega Menu Item 3</a></li>
-                                                <li><a href="#">Mega Menu Item 4</a></li>
-                                                <li><a href="#">Mega Menu Item 5</a></li>
+                                                <li class="title"></li>
+                                                @foreach (Helper::sourcesMegamenu()[3] as $source)
+                                                    <li><a href="/news?source={{$source['id']}}">{{$source['name']}}</a></li>
+                                                @endforeach
                                             </ul>
                                         </div>
                                     <span class="dd-trigger"></span><span class="dd-arrow"></span>
