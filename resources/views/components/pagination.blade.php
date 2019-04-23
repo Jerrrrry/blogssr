8efd7fd91d53b33a67b8fc276af21f4a38880b83
Yubin QIU <Yubin.Qiu@kp.org>
2019-04-23 22:15:37 +0000

    yubin:update

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -36,6 +36,25 @@
     <p>page <strong>{{ $pagination['current'] }}</strong> out of <strong>{{ $pagination['total'] }}</strong></p>
   </nav>
 </div>
+@elseif($pagination['type']=='heros')
+<div class="col-12 col-lg-12">
+  <nav style="margin: 2rem 0 0;text-align: center;">
+    <ul style="display: flex;justify-content: center;margin-bottom: 1rem;">
+      @if($pagination['current']!==1)
+        <li style="margin-left: 1rem;">
+          <a href="/heros?page={{$pagination['current']-1}}">前一页</a>
+        </li>
+      @endif
+      @if($pagination['total']!==$pagination['current']&&$pagination['total']!=='1')
+        <li style="margin-left: 1rem;">
+          <a href="/heros?page={{$pagination['current']+1}}">下一页</a>
+        </li>
+      @endif
+    </ul>
+
+    <p>page <strong>{{ $pagination['current'] }}</strong> out of <strong>{{ $pagination['total'] }}</strong></p>
+  </nav>
+</div>
 @else<!--if not specific, will use default pagination for posts-->
 <div class="col-12 col-lg-12">
   <nav style="margin: 2rem 0 0;text-align: center;">
