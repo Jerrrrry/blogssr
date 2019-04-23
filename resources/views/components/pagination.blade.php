c808788aaaf4448d1f0f2b09bfdb1eb0c944159f
Yubin QIU <Yubin.Qiu@kp.org>
2019-04-23 22:17:03 +0000

    yubin:updste

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -36,18 +36,18 @@
     <p>page <strong>{{ $pagination['current'] }}</strong> out of <strong>{{ $pagination['total'] }}</strong></p>
   </nav>
 </div>
-@elseif($pagination['type']=='heros')
+@elseif($pagination['type']=='marvel-heros')
 <div class="col-12 col-lg-12">
   <nav style="margin: 2rem 0 0;text-align: center;">
     <ul style="display: flex;justify-content: center;margin-bottom: 1rem;">
       @if($pagination['current']!==1)
         <li style="margin-left: 1rem;">
-          <a href="/heros?page={{$pagination['current']-1}}">前一页</a>
+          <a href="/marvel-heros?page={{$pagination['current']-1}}">前一页</a>
         </li>
       @endif
       @if($pagination['total']!==$pagination['current']&&$pagination['total']!=='1')
         <li style="margin-left: 1rem;">
-          <a href="/heros?page={{$pagination['current']+1}}">下一页</a>
+          <a href="/marvel-heros?page={{$pagination['current']+1}}">下一页</a>
         </li>
       @endif
     </ul>
