12cfe7c06bdbf5fc9c30d83fe44e9a822bbbf4ec
Yubin QIU <Yubin.Qiu@kp.org>
2019-05-19 10:47:24 -0700

    yubin:update moviews

--- resources/views/components/topmovies.blade.php
+++ resources/views/components/topmovies.blade.php
@@ -1,6 +1,11 @@
 <div class="container">
     <div class="row">
-        <h2>Full Movies to Watch</h2>
+         <h2>Full Movies to Watch</h2>
+    </div>
+
+</div>
+<div class="container">
+    <div class="row">
         @foreach ($posts as $post)
             @component('components.post',['post' => $post])@endcomponent
         @endforeach
