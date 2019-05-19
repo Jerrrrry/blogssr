82e110f3c551f8089cb9ed2d9f225da7da7971d0
Yubin QIU <Yubin.Qiu@kp.org>
2019-05-19 10:49:01 -0700

    yubin:update the hr

--- resources/views/components/homearticles.blade.php
+++ resources/views/components/homearticles.blade.php
@@ -1,5 +1,13 @@
 <div class="container">
     <div class="row">
+         <h2>Delicious</h2>
+         <hr>
+    </div>
+
+</div>
+
+<div class="container">
+    <div class="row">
       @foreach ($posts as $post)
         @component('components.post',['post' => $post])@endcomponent
       @endforeach
--- resources/views/components/topmovies.blade.php
+++ resources/views/components/topmovies.blade.php
@@ -1,6 +1,7 @@
 <div class="container">
     <div class="row">
          <h2>Full Movies to Watch</h2>
+         <hr>
     </div>
 
 </div>
