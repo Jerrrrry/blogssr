f2c6ee58ee2a1abebbe979f18bd953456d0d5bae
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-18 01:29:52 -0700

    yubin:download pg for ca

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-      <a href="https://tracking.cannaffiliate.com/aff_c?offer_id=12&aff_id=1215&file_id=78" target="_blank"><img src="https://media.go2speed.org/brand/files/cannaffiliate/12/20190807124555-728x90.jpg" width="728" height="90"/></a>
+      <a href="https://tracking.cannaffiliate.com/aff_c?offer_id=12&aff_id=1215&file_id=78" target="_blank"><img src="/img/ca/cablog.jpg" width="728" height="90"/></a>
     </div>
     <div class="row">
       @foreach ($posts as $post)
