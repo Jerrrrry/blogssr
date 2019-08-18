bb7bc79637671fe5b00756607b93e6fa2d2dcfc3
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-18 01:22:21 -0700

    yubin:update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -24,25 +24,28 @@
     </div>
     <div class="container">
         <div class="row">
-          <div class="col-12 col-lg-12">
-              <!-- Single Blog Area  -->
-              <div class="single-blog-area blog-style-2 mb-50">
-                  <!-- Blog Content -->
-                  <div class="single-blog-content">
-                      <h1><a href="#" class="post-headline mb-0">{{$post['post']['title']['rendered']}}</a></h1>
+            <div class="col-12 col-lg-12">
+                <a href="https://tracking.cannaffiliate.com/aff_c?offer_id=12&aff_id=1215&file_id=78" target="_blank"><img src="https://media.go2speed.org/brand/files/cannaffiliate/12/20190807124555-728x90.jpg" width="728" height="90" border="0" /></a><img src="https://tracking.cannaffiliate.com/aff_i?offer_id=12&file_id=78&aff_id=1215" width="1" height="1" />
+            </div>
+            <div class="col-12 col-lg-12">
+                <!-- Single Blog Area  -->
+                <div class="single-blog-area blog-style-2 mb-50">
+                    <!-- Blog Content -->
+                    <div class="single-blog-content">
+                        <h1><a href="#" class="post-headline mb-0">{{$post['post']['title']['rendered']}}</a></h1>
 
-                      {!!html_entity_decode($post['post']['content']['rendered'])!!}
-                  </div>
-              </div>
+                        {!!html_entity_decode($post['post']['content']['rendered'])!!}
+                    </div>
+                </div>
 
-          </div>
+            </div>
 
-          <div class="col-12 col-md-4 col-lg-12">
-              <div class="post-sidebar-area">
-                  @component('components.tags',['tags' => $post['tags']])@endcomponent
+            <div class="col-12 col-md-4 col-lg-12">
+                <div class="post-sidebar-area">
+                    @component('components.tags',['tags' => $post['tags']])@endcomponent
 
-              </div>
-          </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 <div class="container">
     <div class="row">
+      <a href="https://tracking.cannaffiliate.com/aff_c?offer_id=12&aff_id=1215&file_id=78" target="_blank"><img src="https://media.go2speed.org/brand/files/cannaffiliate/12/20190807124555-728x90.jpg" width="728" height="90" border="0" /></a><img src="https://tracking.cannaffiliate.com/aff_i?offer_id=12&file_id=78&aff_id=1215" width="1" height="1" />
+    </div>
+    <div class="row">
       @foreach ($posts as $post)
         @component('components.post',['post' => $post])@endcomponent
       @endforeach
